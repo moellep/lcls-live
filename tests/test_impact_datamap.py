b829e5d62a936b286046a25645ccbce470793e94
Paul Moeller <moeller@radiasoft.net>
2026-03-05 16:48:46 +0000

    updated impact pv test

--- tests/test_impact_datamap.py
+++ tests/test_impact_datamap.py
@@ -1,4 +1,4 @@
-from lcls_live.datamaps.impact import get_impact_datamaps
+from lcls_live.datamaps.impact import as_impact, get_impact_datamaps
 
 
 def test_cu_inj():
@@ -18,6 +18,40 @@ def test_cu_inj():
             "BPMS:IN20:221:TMIT1H": ["", "total_charge", 1.602176634e-19, "C"],
         },
     )
+
+
+def test_cu_inj_pv_values():
+    pv_values = {
+        "SOLN:IN20:121:BACT": 0.4828935473956681,
+        "QUAD:IN20:121:BACT": 0.0050002613789995485,
+        "QUAD:IN20:122:BACT": 0.010000438740486494,
+        "ACCL:IN20:300:L0A_ADES": 59.5,
+        "ACCL:IN20:300:L0A_PDES": 0.0,
+        "ACCL:IN20:400:L0B_ADES": 69.43932268751304,
+        "ACCL:IN20:400:L0B_PDES": -2.5,
+        "QUAD:IN20:361:BACT": -3.483635757505783,
+        "QUAD:IN20:371:BACT": 2.6903314399175904,
+        "QUAD:IN20:425:BACT": -1.2642457321297758,
+        "BPMS:IN20:221:TMIT1H": 1537355904.0,
+        "BPMS:IN20:221:X1H": 0.13991613686084747,
+        "YCOR:IN20:952:BACT": 0.0008449115537848605,
+    }
+    r = {}
+    for name, dm in get_impact_datamaps("cu_inj").items():
+        r.update(as_impact(dm, pv_values))
+    assert r == {
+        ":total_charge": 2.4631157075307476e-10,
+        "QA01:b1_gradient": 3.22558866426689,
+        "QA02:b1_gradient": -2.491047629483575,
+        "QE01:b1_gradient": 1.170597900087386,
+        "SOL1:solenoid_field_scale": 0.24833883322155495,
+        "L0A_phase:dtheta0_deg": 0.0,
+        "L0B_phase:dtheta0_deg": -2.5,
+        "L0A_scale:voltage": 59500000.0,
+        "L0B_scale:voltage": 69439322.68751304,
+        "SQ01:b1_gradient": -0.0047621136859459495,
+        "CQ01:b1_gradient": -0.002381076847142642,
+    }, f"unexpected values from as_impact(): {r}"
 
 
 def test_sc_inj():
